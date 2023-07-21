d2591d8585f73870e0cee6599a8439f8ceb903f7
valenn0101 <veroncaceresvalentin@gmail.com>
2023-07-21 20:34:11 -0300

    Feat: Clean-files
    Se eliminaron archivos que no se utilizaran

--- src/main.tsx
+++ src/main.tsx
@@ -4,26 +4,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import About from "./pages/About";
-import Contact from "./pages/Contact";
 import Error from "./pages/Error";
 import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />, // Componente que queremos cargar
-    errorElement: <Error />,
-    children: [
-      {
-        path: "contacts/:id", //Rutas dinamicas
-        element: <Contact />,
-      },
-    ],
-  },
-  {
-    path: "/about",
-    element: <About />,
+    element: <Home />,
     errorElement: <Error />,
   },
 ]);
